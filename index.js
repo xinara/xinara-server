094e12ffcdce939c6edcb79aaa4d2af391cc69f8
aza <fake@email.com>
2015-04-18 19:42:14 -0300

    chequear que el mensaje no tenga keyid del destinatario

--- index.js
+++ index.js
@@ -25,19 +25,27 @@ expire = 3 * 24 * 60 * 60 * 1000;
 app.post('/', function (req, res) {
   armored_content = req.body.toString("utf8");
   log.debug(armored_content);
+
   pgp_content = openpgp.message.readArmored(armored_content);
+  pgp_key = new Buffer(pgp_content.packets[0].publicKeyId.bytes).toString('hex');
+  log.debug(pgp_key);
 
   openpgp.decryptMessage(pkey, pgp_content).then(function(content) {
     ok = openpgp.message.readArmored(content);
-    log.debug(ok);
+    key_id = new Buffer(ok.packets[0].publicKeyId.bytes).toString('hex');
+    log.debug(key_id);
 
-    now_date = new Date().getTime();
-    available_date = Math.ceil(now_date / batch) * batch;
-    log.debug(content);
-    log.debug(now_date);
-    log.debug(available_date);
-    c.zadd(prefix + 'posts', available_date, content);
-    res.send("OK\n");
+    if ( key_id != '0000000000000000' ) {
+      res.send("ERR\n");
+    } else {
+      now_date = new Date().getTime();
+      available_date = Math.ceil(now_date / batch) * batch;
+      log.debug(content);
+      log.debug(now_date);
+      log.debug(available_date);
+      c.zadd(prefix + 'posts', available_date, content);
+      res.send("OK\n");
+    }
   }).catch(function(error) {
     res.send("ERR\n");
   });
